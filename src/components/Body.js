8aeef89ad063fb8699c6e034fcff2d5a19d06faa
nuru018 <nnpanda.1996@gmail.com>
2024-04-15 22:30:38 +0530

    Episode -06

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import resList from "../utils.js/mockdata"
 import RestaurantCard from "./RestaurantCard"
+import Shimmer from "./Shimmer"
 
 const Body = () => {
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList)
-    return (
+    const [listOfRestaurants, setListOfRestaurants] = useState([])
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
+        const response = await data.json()
+
+        console.log(response)
+        setListOfRestaurants(response?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+    // Conditional Rendering
+
+    // if(listOfRestaurants.length===0){
+    //     return <Shimmer />
+    // }
+
+    return listOfRestaurants.length===0 ? <Shimmer /> : (
         <div className='body'>
             <div className='btn'>
                 <button className="filtered-btn"
---
+++ src/components/Shimmer.js
@@ -0,0 +1,17 @@
+const Shimmer = () => {
+    return (
+    <div className="shimmer-container">
+        <h4 className="shimmer-card"></h4>
+        <h4 className="shimmer-card"></h4>
+        <h4 className="shimmer-card"></h4>
+        <h4 className="shimmer-card"></h4>
+        <h4 className="shimmer-card"></h4>
+        <h4 className="shimmer-card"></h4>
+        <h4 className="shimmer-card"></h4>
+        <h4 className="shimmer-card"></h4>
+        <h4 className="shimmer-card"></h4>
+    </div>
+    )
+}
+
+export default Shimmer
